e906ee54ed1363c9c9c2ea8ddb8492a66f5a2304
Subrata <chandrasinghasubrata@gmail.com>
2025-05-14 16:19:24 +0600

    commit

---
+++ android/app/src/main/kotlin/com/example/set_ab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.set_ab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+void main() {
+  Set<int> setA = {1, 2, 3, 4, 5};
+  Set<int> setB = {4, 5, 6, 7, 8};
+
+  for (int element in setA) {
+    print(element);
+  }
+
+  for (int element in setB) {
+    print(element);
+  }
+}
