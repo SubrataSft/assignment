04706e3185a087a5cfe2c6f1cdd456f6550d9796
Subrata <chandrasinghasubrata@gmail.com>
2025-05-21 08:37:59 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 
-void main() {
-  Set<int> setA = {1, 2, 3, 4, 5};
-  Set<int> setB = {4, 5, 6, 7, 8};
-
-  for (int element in setA) {
-    print(element);
-  }
-
-  for (int element in setB) {
-    print(element);
-  }
+void main(){
+  List<int> numbers = [16,3,1,17,30,6];
+  numbers.sort();
+  List numbersReverse = numbers.reversed.toList();
+  print(numbersReverse);
 }
