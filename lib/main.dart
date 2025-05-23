e8db11e6c027d96ae0672276ae469aaff016e479
Subrata <chandrasinghasubrata@gmail.com>
2025-05-23 11:44:29 +0600

    maxNumber

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-
-void main(){
-  List<int> numbers = [16,3,1,17,30,6];
-  numbers.sort();
-  List numbersReverse = numbers.reversed.toList();
-  print(numbersReverse);
+void main() {
+  List<int> numbers = [16, 3, 1, 17, 30, 6];
+  int maxNumber = numbers.reduce((x,y) => x > y ? x:y);
+  print(maxNumber);
 }
+
